efa7bf29a14e9abce4cc1ef6c16dbbf5e951d05a
qizhen <lim.qizhen@gmail.com>
2022-02-27 22:04:10 +0800

    Update settings.py

--- tys/tys/settings.py
+++ tys/tys/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-35=3(5fchekz#6udg0ebk$=nfb6_j+^83g36t1ev_u7xukr+zs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
